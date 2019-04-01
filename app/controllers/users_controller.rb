2b808162460f7e0f5abe58f24504df8323649e85
Gabriel Aragon <thirtythreebananas@gmail.com>
2019-04-01 14:22:43 -0400

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,13 @@ class UsersController < ApplicationController
 
   def new
   	@user = User.new
+
   end
 
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
   		flash[:success] = "Welcome to this Lousy App!"
   		redirect_to user_url(@user) #or just redirect_to @user
   	else
